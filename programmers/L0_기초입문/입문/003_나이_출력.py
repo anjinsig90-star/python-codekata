f64fa55d12c62c1b3562e737bc52d3e06bcc182b
anjinsig90-star <anjinsig90@gmail.com>
2026-02-05 09:49:36 +0900

    [프로그래머스] L0 - 나이 출력 (#3)

---
+++ programmers/L0_기초입문/입문/003_나이_출력.py
@@ -0,0 +1,10 @@
+# 나이 출력
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120820
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 05. 09:49:36
+
+def solution(age):
+    result = 2023 -age
+    return result
