db9771ce5137efecf827f22343bb21f4775076c6
anjinsig90-star <anjinsig90@gmail.com>
2026-02-06 09:32:41 +0900

    [프로그래머스] L0 - 각도기 (#9)

---
+++ programmers/L0_기초입문/입문/009_각도기.py
@@ -0,0 +1,16 @@
+# 각도기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120829
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 06. 09:32:42
+
+def solution(angle):
+    if (angle>0) & (angle<90) : 
+        return 1
+    elif angle == 90:
+        return 2
+    elif (angle>90) & (angle<180) : 
+        return 3
+    elif angle == 180:
+        return 4
