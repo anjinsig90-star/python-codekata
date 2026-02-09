ed6294384d9a4c9a5b7ea180ea47fd236e5ef5df
anjinsig90-star <anjinsig90@gmail.com>
2026-02-09 09:49:38 +0900

    [프로그래머스] L0 - 편지 (#15)

---
+++ programmers/L0_기초입문/입문/015_편지.py
@@ -0,0 +1,9 @@
+# 편지
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120898
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 09. 09:49:41
+
+def solution(message):
+    return len(message)*2
