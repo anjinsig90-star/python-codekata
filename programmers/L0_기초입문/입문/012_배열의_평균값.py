5a8aed40f0708f208e369f9c095821553dee4c68
anjinsig90-star <anjinsig90@gmail.com>
2026-02-09 09:38:45 +0900

    [프로그래머스] L0 - 배열의 평균값 (#12)

---
+++ programmers/L0_기초입문/입문/012_배열의_평균값.py
@@ -0,0 +1,9 @@
+# 배열의 평균값
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120817
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 09. 09:38:48
+
+def solution(numbers):
+    return sum(numbers)/len(numbers)
