45ae7a70a6ba26d235abbeca0a836fca7b5941f3
anjinsig90-star <anjinsig90@gmail.com>
2026-02-11 09:48:32 +0900

    [프로그래머스] L0 - 삼각형의 완성조건 (1) (#20)

---
+++ programmers/L0_기초입문/입문/020_삼각형의_완성조건__1_.py
@@ -0,0 +1,12 @@
+# 삼각형의 완성조건 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120889
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 11. 09:48:32
+
+def solution(sides):
+    sides.sort()
+    if sides[-1]>=sides[-2]+sides[-3]:
+        return 2
+    else : return 1
