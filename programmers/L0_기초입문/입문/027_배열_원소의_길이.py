e04154644668b868f0449daf8beff384bae49e94
anjinsig90-star <anjinsig90@gmail.com>
2026-02-13 09:46:29 +0900

    [프로그래머스] L0 - 배열 원소의 길이 (#27)

---
+++ programmers/L0_기초입문/입문/027_배열_원소의_길이.py
@@ -0,0 +1,9 @@
+# 배열 원소의 길이
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120854
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 13. 09:46:30
+
+def solution(strlist):
+    return [ len(x) for x in strlist]
