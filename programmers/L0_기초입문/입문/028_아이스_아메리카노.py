165c7b2a1f29279f70941b0150b65ee7feb4514f
anjinsig90-star <anjinsig90@gmail.com>
2026-02-13 09:48:30 +0900

    [프로그래머스] L0 - 아이스 아메리카노 (#28)

---
+++ programmers/L0_기초입문/입문/028_아이스_아메리카노.py
@@ -0,0 +1,9 @@
+# 아이스 아메리카노
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120819
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 13. 09:48:31
+
+def solution(money):
+    return [int(money/5500), money%5500]
