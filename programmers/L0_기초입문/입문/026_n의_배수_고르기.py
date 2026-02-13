8274712c3df22fe4d7198f7e41656fb2e22e9a3c
anjinsig90-star <anjinsig90@gmail.com>
2026-02-13 09:43:34 +0900

    [프로그래머스] L0 - n의 배수 고르기 (#26)

---
+++ programmers/L0_기초입문/입문/026_n의_배수_고르기.py
@@ -0,0 +1,9 @@
+# n의 배수 고르기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120905
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 13. 09:43:34
+
+def solution(n, numlist):
+    return [x for x in numlist if x%n==0 ]
