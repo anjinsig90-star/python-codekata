5db5e15492241048bdaab9c9d84068a4860ca8d4
anjinsig90-star <anjinsig90@gmail.com>
2026-02-26 09:49:45 +0900

    [프로그래머스] L0 - 배열 두 배 만들기 (#37)

--- programmers/L0_기초입문/입문/037_배열_두_배_만들기.py
+++ programmers/L0_기초입문/입문/037_배열_두_배_만들기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120809
 # 알고리즘: 기초
 # 작성자: 안진식
-# 작성일: 2026. 02. 26. 09:49:20
+# 작성일: 2026. 02. 26. 09:49:46
 
 def solution(numbers):
     return [i*2 for i in numbers]
