724e0ced924ca0af9c7ea8b345416a2e82db9be6
anjinsig90-star <anjinsig90@gmail.com>
2026-02-27 09:35:06 +0900

    [프로그래머스] L0 - 중앙값 구하기 (#39)

---
+++ programmers/L0_기초입문/입문/039_중앙값_구하기.py
@@ -0,0 +1,10 @@
+# 중앙값 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120811
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 27. 09:35:06
+
+import numpy as np
+def solution(array):
+    return np.median(array)
