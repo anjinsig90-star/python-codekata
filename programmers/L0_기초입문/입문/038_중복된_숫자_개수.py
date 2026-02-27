e41827e619cd12f4b3bcae1c3ef68ea17e4821eb
anjinsig90-star <anjinsig90@gmail.com>
2026-02-27 09:31:47 +0900

    [프로그래머스] L0 - 중복된 숫자 개수 (#38)

---
+++ programmers/L0_기초입문/입문/038_중복된_숫자_개수.py
@@ -0,0 +1,13 @@
+# 중복된 숫자 개수
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120583
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 27. 09:31:47
+
+def solution(array, n):
+    sum=0
+    for i in array:       
+        if i==n:
+            sum+=1
+    return sum
