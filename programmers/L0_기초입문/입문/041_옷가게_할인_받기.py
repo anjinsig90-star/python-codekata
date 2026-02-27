b60961a63cded73d777da813421d114283254dfc
anjinsig90-star <anjinsig90@gmail.com>
2026-02-27 09:52:13 +0900

    [프로그래머스] L0 - 옷가게 할인 받기 (#41)

---
+++ programmers/L0_기초입문/입문/041_옷가게_할인_받기.py
@@ -0,0 +1,15 @@
+# 옷가게 할인 받기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120818
+# 알고리즘: 기초
+# 작성자: 안진식
+# 작성일: 2026. 02. 27. 09:52:12
+
+def solution(price):
+    if price>=500000:
+        return int(price*0.8)
+    elif price>=300000:
+        return int(price*0.9)
+    elif price>=100000:
+        return int(price*0.95)
+    else : return price
